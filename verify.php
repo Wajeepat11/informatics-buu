7265449b41f6994aa9921f95f129b891cc080fea
wajeepat <31946215+Wajeepat11@users.noreply.github.com>
2019-01-24 15:53:47 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'sf2/JMj8FDC4zbIbFtcAtRaQqNqV74Bzana2/88SRv2rZuwZyyQaBdy1RsJ8xUMB+J38BP9WW36F9REKpFa5Uhob59s90gU5kbX/is/hZdotjxuGtmRN8GE6J0z6XJYTmwZW53v4erTQfYg/oD8bGQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
